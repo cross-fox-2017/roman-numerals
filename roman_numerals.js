6e8aa731d85d267d70f687c651b8795645ae4177
Brandon &   "name": null <pair+endy.triyan@hacktiv8.com>
2017-01-02 15:37:33 +0700

    rubah romawi new

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,8 +1,10 @@
 function to_roman_old(n) {
+  /*
   // cari jumlah digit parameter
   var number = String(n),
       tampung = "",
       romawi = ['I', 'V', 'X', 'L', 'C', 'D', "M"];
+      arabic = [1, 5, 10, 50, 100, 500, 1000];
 
   // jika jumlah digit angka == 1
   if(number.length == 1 && number != 0) {
@@ -46,20 +48,111 @@ function to_roman_old(n) {
 
 
   }
+  if(number.length == 2 && number >= 50) {
+    // cek digit pertama
+    tampung += romawi[3];
+    if (number[1] == )
 
 
+    // cek digit kedua < 5
 
+
+  }
+  */
+  var number = n,
+      index_number = String(n),
+      hitung = number,
+      tampung = "",
+      roman = ["I", "V", "X", "L", "C", "D", "M"];
+
+  while(number > 0) {
+    if ( number >= 1000 && number <= 3000 ) {
+      number = number - 1000;
+      tampung += "M";
+    }
+
+    else if ( number >= 500 && number <= 999 ) {
+      number = number - 500;
+      tampung += 'D';
+    }
+
+    else if ( number >= 100 && number <= 499) {
+      number = number - 100;
+      tampung += 'C';
+    }
+
+    else if ( number >= 50 && number <= 99) {
+      number = number - 50;
+      tampung += "L";
+    }
+
+    else if ( number >= 10 && number <= 49) {
+      number = number - 10;
+      tampung += "X";
+    }
+
+    else if ( number >= 5 && number <= 9) {
+      number = number - 5;
+      tampung += 'V';
+    }
+
+    else if ( number >= 1 && number <= 4) {
+      number = number - 1;
+      tampung += 'I';
+    }
+  }
+  return tampung
 }
 
-
-to_roman_old(17);
 
 function to_roman(n) {
   // your implementation code here
+  var number = n,
+      index_number = String(n),
+      hitung = number,
+      tampung = "",
+      roman = ["I", "V", "X", "L", "C", "D", "M"];
+
+  while(number > 0) {
+    if ( number >= 1000 && number <= 3000 ) {
+      number = number - 1000;
+      tampung += "M";
+    }
+
+    else if ( number >= 500 && number <= 999 ) {
+      number = number - 500;
+      tampung += 'D';
+    }
+
+    else if ( number >= 100 && number <= 499) {
+      number = number - 100;
+      tampung += 'C';
+    }
+
+    else if ( number >= 50 && number <= 99) {
+      number = number - 50;
+      tampung += "L";
+    }
+
+    else if ( number >= 10 && number <= 49) {
+      number = number - 10;
+      tampung += "X";
+    }
+
+    else if ( number >= 5 && number <= 9) {
+      number = number - 5;
+      tampung += 'V';
+    }
+
+    else if ( number >= 1 && number <= 4) {
+      number = number - 1;
+      tampung += 'I';
+    }
+  }
+  return tampung
 }
 
 // Drive code
-/*
 console.log("My totally sweet testing script\n");
 console.log("input | expected | actual");
 console.log("———|—————|———");
@@ -77,4 +170,3 @@ console.log("9     | IX       | ", to_roman(9));
 console.log("13    | XIII     | ", to_roman(13));
 console.log("1453  | MCDLIII  | ", to_roman(1453));
 console.log("1646  | MDCXLVI  | ", to_roman(1646));
-*/
