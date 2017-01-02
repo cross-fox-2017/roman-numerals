24bcbecf5238cecc50403bacc015397d1b501bb7
Ida Bagus Chahya Dhegana & yoma sofwan <pair+dgana.yomaswn@hacktiv8.com>
2017-01-02 13:41:14 +0700

    Done roman_numerals.js

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,9 +1,160 @@
 function to_roman_old(n) {
   // your code here
+  let str = '';
+  let romanD = 0;
+  let sisa = 0;
+
+  // Get Thousand Roman
+  romanD = Math.floor(n / 1000);
+
+  for(let i = 0 ; i < romanD ; i++){
+    str += 'M';
+  }
+  sisa = n % 1000;
+
+  // Get 500 Roman
+  romanD = Math.floor(sisa / 500);
+  for(let i = 0 ; i < romanD ; i++){
+    str += 'D';
+  }
+  sisa = n % 500;
+
+  // Get 100 Roman
+  romanC = Math.floor(sisa / 100);
+  if(romanC > 3){
+    str += 'CD';
+  }
+  else {
+    for(let i = 0 ; i < romanC ; i++){
+      str += 'C';
+    }
+  }
+  sisa = n % 100;
+
+  // Get 50 Roman
+  romanL = Math.floor(sisa / 50);
+  for(let i = 0 ; i < romanL ; i++){
+    str += 'L';
+  }
+  sisa = n % 50;
+
+  // Get 10 Roman
+  romanX = Math.floor(sisa / 10);
+  if(romanX > 3){
+    str += 'XL';
+  }
+  else {
+    for(let i = 0 ; i < romanX ; i++){
+      str += 'X';
+    }
+  }
+
+  sisa = n % 10;
+  // console.log(romanL, sisa);
+
+  // Get 5 Roman
+  romanV = Math.floor(sisa / 5);
+
+  for(let i = 0 ; i < romanV ; i++){
+    str += 'V';
+  }
+  sisa = n % 5;
+
+  // Get Ones Roman
+  romanI = Math.floor(sisa / 1);
+  for(let i = 0 ; i < romanI ; i++){
+    str += 'I';
+  }
+  sisa = n % 1;
+
+  return str;
 }
 
 function to_roman(n) {
-  // your implementation code here
+  // your code here
+  let str = '';
+  let romanD = 0;
+  let sisa = 0;
+
+  // Get Thousand Roman
+  romanD = Math.floor(n / 1000);
+
+  for(let i = 0 ; i < romanD ; i++){
+    str += 'M';
+  }
+  sisa = n % 1000;
+
+  // Get 500 Roman
+  romanD = Math.floor(sisa / 500);
+  for(let i = 0 ; i < romanD ; i++){
+    str += 'D';
+  }
+  sisa = n % 500;
+
+  // Get 100 Roman
+  romanC = Math.floor(sisa / 100);
+  if(romanC > 3){
+    str += 'CD';
+  }
+  else {
+    for(let i = 0 ; i < romanC ; i++){
+      str += 'C';
+    }
+  }
+  sisa = n % 100;
+
+  // Get 50 Roman
+  romanL = Math.floor(sisa / 50);
+  for(let i = 0 ; i < romanL ; i++){
+    str += 'L';
+  }
+  sisa = n % 50;
+
+  // Get 10 Roman
+  romanX = Math.floor(sisa / 10);
+  if(romanX > 3){
+    str += 'XL';
+  }
+  else {
+    for(let i = 0 ; i < romanX ; i++){
+      str += 'X';
+    }
+  }
+
+  sisa = n % 10;
+  // console.log(romanL, sisa);
+
+  // Get 5 Roman
+  romanV = Math.floor(sisa / 5);
+  romanVCustom = sisa / 5;
+
+  if(romanVCustom > 1.6){
+    str += 'IX';
+  }
+  else {
+    for(let i = 0 ; i < romanV ; i++){
+      str += 'V';
+    }
+
+    sisa = n % 5;
+
+    // Get Ones Roman
+    romanI = Math.floor(sisa / 1);
+
+    if(romanI > 3){
+      str += 'IV';
+    }
+    else {
+      for(let i = 0 ; i < romanI ; i++){
+        str += 'I';
+      }
+    }
+    sisa = n % 1;
+  }
+
+
+
+  return str;
 }
 
 // Drive code
